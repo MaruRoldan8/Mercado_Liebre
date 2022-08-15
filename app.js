32157ee698a7c1ef724557575051a377a2cccf32
MaruRoldan8 <mariadelosangeles.rol8@gmail.com>
2022-08-15 16:54:12 -0300

    Agregando estilos, clase 12

--- app.js
+++ app.js
@@ -30,3 +30,21 @@ app.get("/ingresa", (req, res) => {
 app.get("/mis-compras", (req, res) => {
     res.sendFile(path.resolve(__dirname, "./views/mis-compras.html"));
 });
+
+
+
+//Paleta de colores
+//Amarillo: #EAC926;
+//Azul: #03264C;
+//Celeste: #1259c3;
+//Gris oscuro: #666;
+//Gris claro: #ebebeb;
+//Verde: #39b54a;
+
+
+//Tamaños Tipográficos
+//Cuerpos de texto: 12px
+//Titulares (Basado en tu última visita/Ofertas): 24px
+//Precios: 18px
+//Descuentos: 16px
+//Descripciones: 16px
